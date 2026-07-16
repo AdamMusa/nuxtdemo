03548c7843155dd53ce998bdb9dd22f27a3c4d5d
AdamMusa <adammusa2222@gmail.com>
2026-07-16 02:23:21 -0400

    Add app/app.vue

---
+++ app/app.vue
@@ -0,0 +1,1 @@
+<template><main><h1>Nuxt.js on Alkimist</h1><p>Deployment is healthy.</p></main></template>\n<style>body{font-family:system-ui;margin:4rem;background:#071a16;color:#d8fff3}h1{color:#00dc82}</style>\n
